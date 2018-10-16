355226c87338b07abad1621cea6a42058251c884
Garrett Brown <themagnificentmrb@gmail.com>
2018-10-15 23:57:14 -0700

    temp: video work

--- examples/kodi/build_steamlink.sh
+++ examples/kodi/build_steamlink.sh
@@ -47,11 +47,11 @@ if [ "${TOP}/kodi.patch" -nt "${BUILD}/.patch-applied" ]; then
 	touch "${BUILD}/.patch-applied"
 fi
 
-#if [ ! -f "${SRC}/tools/depends/configure" ]; then
+if [ ! -f "${SRC}/tools/depends/configure" ]; then
 	pushd "${SRC}/tools/depends"
 	./bootstrap || exit 1
 	popd
-#fi
+fi
 
 #
 # Configure build dependencies
@@ -68,7 +68,7 @@ fi
 
 DEPS_INSTALL_PATH="${MARVELL_SDK_PATH}/kodi-deps/${SOC_BUILD}-${BUILD_MODE}"
 
-#if [ ! -f "${SRC}/tools/depends/Makefile.include" ]; then
+if [ ! -f "${SRC}/tools/depends/Makefile.include" ]; then
 	pushd "${SRC}/tools/depends"
 	./configure \
 	    `[ ${BUILD_MODE} == debug ] && echo "--enable-debug" || echo "--disable-debug"` \
@@ -76,7 +76,7 @@ DEPS_INSTALL_PATH="${MARVELL_SDK_PATH}/kodi-deps/${SOC_BUILD}-${BUILD_MODE}"
 	    --with-platform=steamlink \
 	    --with-toolchain="${MARVELL_SDK_PATH}/toolchain" \
 	    --host=${SOC_BUILD} || exit 2
-#fi
+fi
 
 # GMP from the Steam Link SDK conflicts with Kodi
 if [ -f "${MARVELL_ROOTFS}/usr/include/gmp.h" ]; then
@@ -126,7 +126,7 @@ popd
 #make -C target/samba || exit 3
 
 # Build binary add-ons
-make -C target/binary-addons PREFIX="${BUILD}/steamlink/apps/kodi/home/apps/kodi" -j20 ADDONS="peripheral.joystick" || exit 3
+#make -C target/binary-addons PREFIX="${BUILD}/steamlink/apps/kodi/home/apps/kodi" -j20 ADDONS="peripheral.joystick" || exit 3
 
 # All done!
 
@@ -136,7 +136,7 @@ popd
 # Finally build Kodi
 #
 pushd "${SRC}"
-make -C tools/depends/target/cmakebuildsys CMAKE_EXTRA_ARGUMENTS="-DCMAKE_INSTALL_PREFIX=/home/apps/kodi" || exit 5
+#make -C tools/depends/target/cmakebuildsys CMAKE_EXTRA_ARGUMENTS="-DCMAKE_INSTALL_PREFIX=/home/apps/kodi" || exit 5
 pushd build
 make -j${NCPU} || exit 5
 
