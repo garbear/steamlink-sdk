3dd43e392b3c2b499a3ab24f14d9ca3afccebf93
Garrett Brown <themagnificentmrb@gmail.com>
2018-10-16 01:10:17 -0700

    Revert "rv"
    This reverts commit b62eef791615c373474842d0dd24a702654f0ed4.

--- examples/kodi/build_steamlink.sh
+++ examples/kodi/build_steamlink.sh
@@ -58,7 +58,7 @@ fi
 #
 MARVELL_SDK_PATH="$(cd "${TOP}/../.." && pwd)"
 MARVELL_ROOTFS="${MARVELL_SDK_PATH}/rootfs"
-SOC_BUILD=armv7a-unknown-linux-gnueabi
+SOC_BUILD=armv7a-cros-linux-gnueabi
 BUILD_MODE=debug # or release
 
 if [ ! -d "${MARVELL_ROOTFS}" ]; then
